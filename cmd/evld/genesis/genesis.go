c283f389a856aae1cfe0e45d6ba98395de6f1fc0
VictorTrustyDev <me@victortrusty.dev>
2025-04-15 00:11:06 +0700

    add snapshots to genesis

--- cmd/evld/genesis/genesis.go
+++ cmd/evld/genesis/genesis.go
@@ -1,6 +1,11 @@
 package genesis
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +18,49 @@ func Cmd() *cobra.Command {
 
 	cmd.AddCommand(
 		NewImproveGenesisCmd(),
+		NewAddEvmosSnapshotCmd(),
 	)
 
 	return cmd
+}
+
+func generalGenesisUpdateFunc(cmd *cobra.Command, updater func(genesis map[string]json.RawMessage, clientCtx client.Context) error) error {
+	clientCtx := client.GetClientContextFromCmd(cmd)
+	if homeDir := clientCtx.HomeDir; homeDir == "" {
+		return fmt.Errorf("home dir not set")
+	}
+
+	// Load the genesis file
+	genesisFile := fmt.Sprintf("%s/config/genesis.json", clientCtx.HomeDir)
+	genesisData, err := os.ReadFile(genesisFile)
+	if err != nil {
+		return fmt.Errorf("failed to read genesis file: %w", err)
+	}
+
+	// Parse the genesis file
+	var genesis map[string]json.RawMessage
+	err = json.Unmarshal(genesisData, &genesis)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal genesis file: %w", err)
+	}
+
+	// Update
+	err = updater(genesis, clientCtx)
+	if err != nil {
+		return fmt.Errorf("failed to update genesis: %w", err)
+	}
+
+	// Marshal the updated genesis back to JSON
+	updatedGenesisData, err := json.MarshalIndent(genesis, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal updated genesis: %w", err)
+	}
+
+	// Write the updated genesis back to the file
+	err = os.WriteFile(genesisFile, updatedGenesisData, 0o644)
+	if err != nil {
+		return fmt.Errorf("failed to write updated genesis file: %w", err)
+	}
+
+	return nil
 }
---
+++ constants/constants_evmos.go
@@ -0,0 +1,8 @@
+package constants
+
+//goland:noinspection SpellCheckingInspection
+const (
+	SnapshotEvmosMinDenom = "uevmos" // not aevmos since reduced to 6 decimals by snapshot tool
+	SnapshotEvmosExponent = 6
+	SnapshotEvmosSymbol   = "EVMOS"
+)
